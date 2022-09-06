25f0a7807ece133459db8aef17c79e8d98bd3cb8
akram3598786 <akramali03598786@gmail.com>
2022-09-06 18:45:25 +0530

    Building-fron-page

--- travel-city-clone/src/App.jsx
+++ travel-city-clone/src/App.jsx
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './components/Home';
 
 function App() {
   return (
     <div className="App">
        <h1>Travel City Clone</h1>
+       <HomePage/>
     </div>
   );
 }
